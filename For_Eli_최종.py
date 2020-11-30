ce12bfba5a239b9f1bb3d035aee32e7f7fb88db9
dkstlgus7571 <68101236+dkstlgus7571@users.noreply.github.com>
2020-12-01 05:04:34 +0900

    Add files via upload

---
+++ For_Eli_최종.py
@@ -0,0 +1,24 @@
+def for_eli(mat):
+    newmat = []
+    row_idx = list(range(len(mat)))
+    col_idx = len(mat[0])
+    for c in range(col_idx):
+        rows_with_nonzero = [r for r in row_idx if mat[r][c]!=0]
+        if rows_with_nonzero:
+            pivot = rows_with_nonzero[0]
+            row_idx.remove(pivot)
+            newmat.append(mat[pivot])
+            for r in rows_with_nonzero:
+                if r is not pivot:
+                    multiplier = mat[r][c]/mat[pivot][c]
+                    mat[r] = [a-multiplier*b for a,b in zip(mat[r], mat[pivot])]
+
+    for r in row_idx:
+        newmat.append(mat[r])
+    return newmat
+
+mat = [[1,1,1,4],
+       [2,1,-3,1],
+       [-1,-2,1,-4]]
+
+print(for_eli(mat))
